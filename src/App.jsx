652e22344c2c59c6ed80ceeb6e17611f7b5e46ac
Its_Shojib <mdshojib922@gmail.com>
2023-09-21 01:17:38 +0600

    Form Added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import SimpleForm from './Component/SimpleForm/SimpleForm';
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
