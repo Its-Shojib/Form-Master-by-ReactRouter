7cdbfcdb550f5cd870188f3f355dccc50582ac1a
Its_Shojib <mdshojib922@gmail.com>
2023-09-21 01:27:17 +0600

    Email Added

--- src/Component/SimpleForm/SimpleForm.jsx
+++ src/Component/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,17 @@
 
 const SimpleForm = () => {
+    let handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log('form Submitted');
+    }
     return (
         <div>
-            <form >
-                <input type="text" name="" id="" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" placeholder="input name" />
+                <br />
+                <input type="email" name="email" id="" placeholder="input email"/>
                 <br />
                 <input type="submit" value="Submit" />
             </form>
